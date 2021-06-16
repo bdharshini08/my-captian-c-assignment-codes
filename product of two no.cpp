7621f22b136df8a404ece9e222a9314c8c418ba8
bdharshini08 <85825092+bdharshini08@users.noreply.github.com>
2021-06-16 15:53:24 +0530

    Add files via upload
    assignment based on tokens and operators

---
+++ product of two no.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y,product=0.0;
+	cout<<"enter the two numbers:";
+	cin>>x>>y;
+	product=x*y;
+	cout<<"the product of two numbers is:"<<product<<endl;
+	return 0;
+}
